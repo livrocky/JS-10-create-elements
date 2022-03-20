99ff9bdb02440d2c4c6fbbcdd0a78f5d5cb903b4
rokas <rokasra@gmail.com>
2022-03-20 18:32:28 +0200

    JS-11, DOM el kurimas, psl su cuciku

--- js/cao.js
+++ js/cao.js
@@ -1,20 +1,19 @@
-//1. Susikuriame h1 elementą, jame įrašome savo vardą, pastilizuojame, kad elementas būtų raudonas ir pridedame jį prie <body>.
-
+//NAMEE//
 const h1El = document.createElement("h1");
 h1El.textContent = "Rokas";
 h1El.style.color = "red";
+
 document.body.append(h1El);
 
-//2. Sukuriame <ul> elementą, o jame - tris <li> elementus su mašinų brand'ais. <body> turi atsirasti <ul>, kuriame trys <li>
+//Unordered list of cars//
 
 const ulEl = document.createElement("ul");
-
-const li1 = document.createElement("li");
-li1.textContent = "Volvo";
-const li2 = document.createElement("li");
-li2.textContent = "VW";
-const li3 = document.createElement("li");
-li3.textContent = "Audi";
-
-ulEl.append(li1, li2, li3);
 document.body.append(ulEl);
+const liEl1 = document.createElement("li");
+liEl1.textContent = "Audi";
+const liEl2 = document.createElement("li");
+liEl2.textContent = "VW";
+const liEl3 = document.createElement("li");
+liEl3.textContent = "Volvo";
+
+ulEl.append(liEl1, liEl2, liEl3);
